0a5852fb37e659e4a31ae257ce4612b01eabb5c5
RaymondWHZ <wuhaozhen1106@gmail.com>
2025-12-03 18:29:10 -0600

    feat: update dismiss comment button style

--- src/lib/components/sections/document/extensions/comment/CommentNodeView.svelte
+++ src/lib/components/sections/document/extensions/comment/CommentNodeView.svelte
@@ -37,22 +37,21 @@
           </span>
         {/snippet}
       </Tooltip.Trigger>
-      <Tooltip.Content class="max-w-xs p-4 text-base shadow-sm" sideOffset={4}>
-        <div class="flex items-start gap-2">
-          <div class="flex-1">
-            {#if title}
-              <div class="mb-1 text-lg font-semibold">{title}</div>
-            {/if}
-            <Markdown value={comment} />
-          </div>
-          <button
-            class="mt-0.5 shrink-0 rounded p-0.5 text-gray-400 hover:bg-gray-200 hover:text-gray-600"
-            onclick={handleDismiss}
-            title="Remove comment"
-          >
-            <X class="size-4" />
-          </button>
-        </div>
+      <Tooltip.Content
+        class="relative max-w-xs p-4 text-base shadow-sm"
+        sideOffset={4}
+      >
+        <button
+          class="absolute top-1 right-1 rounded p-1 text-gray-400 hover:bg-gray-200 hover:text-gray-600"
+          onclick={handleDismiss}
+          title="Remove this response block"
+        >
+          <X class="size-4" />
+        </button>
+        {#if title}
+          <div class="mb-1 text-lg font-semibold">{title}</div>
+        {/if}
+        <Markdown value={comment} />
       </Tooltip.Content>
     </Tooltip.Root>
   </Tooltip.Provider>
